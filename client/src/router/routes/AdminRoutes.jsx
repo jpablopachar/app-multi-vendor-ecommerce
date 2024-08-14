a33a891fb8bcd195047851fb24b47b935a324d97
jpablopachar <jpablopachar1993@gmail.com>
2024-08-14 15:43:16 -0500

    Diseño de página de Ordenes y componente Paginación

--- client/src/router/routes/AdminRoutes.jsx
+++ client/src/router/routes/AdminRoutes.jsx
@@ -1,4 +1,5 @@
 import { lazy } from "react"
+import Orders from "../../views/admin/Orders"
 
 // eslint-disable-next-line react-refresh/only-export-components
 const AdminDashboard = lazy(() => import('../../views/admin/AdminDashboard'))
@@ -7,6 +8,11 @@ export const adminRoutes = [
   {
     path: '/admin/dashboard',
     element: <AdminDashboard/>,
+    role: 'admin'
+  },
+  {
+    path: '/admin/dashboard/orders',
+    element: <Orders/>,
     role: 'admin'
   }
 ]
