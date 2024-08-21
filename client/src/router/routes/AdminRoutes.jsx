58e9dac51067bad553ffde4afc506053d42a41dd
jpablopachar <jpablopachar1993@gmail.com>
2024-08-21 11:25:24 -0500

    Diseño de las páginas DeactiveSellers, PaymentRequest, SellerDetails y SellerRequest

--- client/src/router/routes/AdminRoutes.jsx
+++ client/src/router/routes/AdminRoutes.jsx
@@ -5,6 +5,10 @@ const AdminDashboard = lazy(() => import('../../views/admin/AdminDashboard'))
 const Category = lazy(() => import('../../views/admin/Category'))
 const Orders = lazy(() => import('../../views/admin/Orders'))
 const Sellers = lazy(() => import('../../views/admin/Sellers'))
+const PaymentRequest = lazy(() => import('../../views/admin/PaymentRequest'))
+const DeactiveSellers = lazy(() => import('../../views/admin/DeactiveSellers'))
+const SellerRequest = lazy(() => import('../../views/admin/SellerRequest'))
+const SellerDetails = lazy(() => import('../../views/admin/SellerDetails'))
 
 export const adminRoutes = [
   {
@@ -26,5 +30,25 @@ export const adminRoutes = [
     path: '/admin/dashboard/sellers',
     element: <Sellers/>,
     role: 'admin'
+  },
+  {
+    path: 'admin/dashboard/payment-request',
+    element : <PaymentRequest/>,
+    role : 'admin'
+  },
+  {
+    path: 'admin/dashboard/deactive-sellers',
+    element : <DeactiveSellers/>,
+    role : 'admin'
+  },
+  {
+    path: 'admin/dashboard/sellers-request',
+    element : <SellerRequest/>,
+    role : 'admin'
+  },
+  {
+    path: 'admin/dashboard/seller/details/:sellerId',
+    element : <SellerDetails/>,
+    role : 'admin'
   }
 ]
