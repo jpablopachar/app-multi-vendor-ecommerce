76f7228e5f9a72ba289760b7059eaddb18a95f19
jpablopachar <jpablopachar1993@gmail.com>
2024-08-21 17:40:54 -0500

    Diseño de página ChatSeller y OrderDetails

--- client/src/router/routes/AdminRoutes.jsx
+++ client/src/router/routes/AdminRoutes.jsx
@@ -9,6 +9,8 @@ const PaymentRequest = lazy(() => import('../../views/admin/PaymentRequest'))
 const DeactiveSellers = lazy(() => import('../../views/admin/DeactiveSellers'))
 const SellerRequest = lazy(() => import('../../views/admin/SellerRequest'))
 const SellerDetails = lazy(() => import('../../views/admin/SellerDetails'))
+const ChatSeller = lazy(() => import('../../views/admin/ChatSeller'))
+const OrderDetails = lazy(() => import('../../views/admin/OrderDetails'))
 
 export const adminRoutes = [
   {
@@ -49,6 +51,16 @@ export const adminRoutes = [
   {
     path: 'admin/dashboard/seller/details/:sellerId',
     element : <SellerDetails/>,
+    role : 'admin'
+  },
+  {
+    path: 'admin/dashboard/chat-sellers',
+    element : <ChatSeller/>,
+    role : 'admin'
+  },
+  {
+    path: 'admin/dashboard/order/details/:orderId',
+    element : <OrderDetails/>,
     role : 'admin'
   }
 ]
