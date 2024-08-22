1ee6fca8abd172a265246f6cdbd3b4ab7a2cb892
jpablopachar <jpablopachar1993@gmail.com>
2024-08-21 20:40:11 -0500

    Diseño de la página AddProduct

--- client/src/router/routes/SellerRoutes.jsx
+++ client/src/router/routes/SellerRoutes.jsx
@@ -1,18 +1,24 @@
 /* eslint-disable react-refresh/only-export-components */
-import { lazy } from "react"
+import { lazy } from 'react'
 
 const Home = lazy(() => import('../../views/Home'))
 const SellerDashboard = lazy(() => import('../../views/seller/SellerDashboard'))
+const AddProduct = lazy(() => import('../../views/seller/AddProduct'))
 
 export const sellerRoutes = [
   {
     path: '/',
-    element: <Home/>,
-    ability: ['admin', 'seller']
+    element: <Home />,
+    ability: ['admin', 'seller'],
   },
   {
     path: '/seller/dashboard',
-    element: <SellerDashboard/>,
-    ability: ['seller']
-  }
+    element: <SellerDashboard />,
+    ability: ['seller'],
+  },
+  {
+    path: '/seller/dashboard/add-product',
+    element: <AddProduct />,
+    ability: ['seller'],
+  },
 ]
