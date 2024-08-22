3792e9568064d4350e9b4a59200610d0ff5d8503
jpablopachar <jpablopachar1993@gmail.com>
2024-08-21 20:28:39 -0500

    Diseño de la pantalla Dashboard para vendedores

--- client/src/router/routes/SellerRoutes.jsx
+++ client/src/router/routes/SellerRoutes.jsx
@@ -1,12 +1,18 @@
+/* eslint-disable react-refresh/only-export-components */
 import { lazy } from "react"
 
-// eslint-disable-next-line react-refresh/only-export-components
 const Home = lazy(() => import('../../views/Home'))
+const SellerDashboard = lazy(() => import('../../views/seller/SellerDashboard'))
 
 export const sellerRoutes = [
   {
     path: '/',
     element: <Home/>,
     ability: ['admin', 'seller']
+  },
+  {
+    path: '/seller/dashboard',
+    element: <SellerDashboard/>,
+    ability: ['seller']
   }
 ]
