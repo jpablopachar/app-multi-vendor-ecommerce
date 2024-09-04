3a7e5802e0ee8c3ee25b121f3a870a9099e00aab
jpablopachar <jpablopachar1993@gmail.com>
2024-09-04 11:54:21 -0500

    Corrección de enrutamiento

--- client-angular/src/app/router/admin.routes.ts
+++ client-angular/src/app/router/admin.routes.ts
@@ -6,7 +6,15 @@ export const AdminRoutes: Routes = [
     loadComponent: () => import('../views/auth/admin-login.component').then(c => c.AdminLoginComponent)
   },
   {
-    path: 'dashboard',
-    loadComponent: () => import('../views/admin/admin-dashboard.component').then(c => c.AdminDashboardComponent)
-  }
+    path: '',
+    loadComponent: () => import('../layout/main.component').then(c => c.MainComponent),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('../views/admin/admin-dashboard.component').then(c => c.AdminDashboardComponent)
+      },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full', }
+    ]
+  },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full', },
 ]
--- client-angular/src/app/router/seller.routes.ts
+++ client-angular/src/app/router/seller.routes.ts
@@ -1,8 +1,13 @@
-import { Routes } from "@angular/router"
+import { Routes } from '@angular/router'
 
 export const SellerRoutes: Routes = [
   {
     path: 'dashboard',
-    loadComponent: () => import('../views/seller/seller-dashboard.component').then(c => c.SellerDashboardComponent)
-  }
-]
+    loadComponent: () =>
+      import('../views/seller/seller-dashboard.component').then(
+        (c) => c.SellerDashboardComponent
+      ),
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full', },
+];
