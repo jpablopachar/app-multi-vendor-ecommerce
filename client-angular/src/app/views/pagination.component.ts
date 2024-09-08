aae9a5045b5362a5508b8a507ce6c61a79b329ca
jpablopachar <jpablopachar1993@gmail.com>
2024-09-07 19:44:14 -0500

    Intento de paginación

--- client-angular/src/app/views/pagination.component.ts
+++ client-angular/src/app/views/pagination.component.ts
@@ -1,10 +1,15 @@
 import { CommonModule } from '@angular/common'
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   EventEmitter,
   Output,
+  Renderer2,
+  ViewChild,
   effect,
+  inject,
   input,
   signal,
 } from '@angular/core'
@@ -16,16 +21,17 @@ import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
   standalone: true,
   imports: [CommonModule, FontAwesomeModule],
   template: `
-    <ng-template #btns>
+    <!-- <ng-template #btns>
       <li
         class="w-[33px] h-[33px] rounded-full flex justify-center items-center cursor-pointer"
       >
         Hola
       </li>
-    </ng-template>
-    <div class="flex gap-3">
+    </ng-template> -->
+    <ul class="flex gap-3" #btns>
       @if ($pageNumber() > 1) {
       <li
+        id="first"
         class="w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-300 text-[#000000] cursor-pointer"
         (click)="setPageNumber.emit($pageNumber() - 1)"
         (keypress)="('')"
@@ -34,9 +40,10 @@ import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
         <fa-icon [icon]="faAngleLeft"></fa-icon>
       </li>
       }
-      <ng-container *ngTemplateOutlet="btns"></ng-container>
+      <!-- <ng-container *ngTemplateOutlet="btns"></ng-container> -->
       @if ($pageNumber() < $totalPage()) {
       <li
+        id="last"
         class="w-[33px] h-[33px] rounded-full flex justify-center items-center bg-slate-300 text-[#000000] cursor-pointer"
         (click)="setPageNumber.emit($pageNumber() + 1)"
         (keypress)="('')"
@@ -45,15 +52,14 @@ import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
         <fa-icon [icon]="faAngleRight"></fa-icon>
       </li>
       }
-    </div>
+    </ul>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PaginationComponent {
-  /* @Input({ required: true }) pageNumber!: number;
-  @Input({ required: true }) totalItem!: number;
-  @Input({ required: true }) parPage!: number;
-  @Input({ required: true }) showItem!: number; */
+export class PaginationComponent implements AfterViewInit {
+  @ViewChild('btns', { static: true })
+  btns!: ElementRef;
+
   $pageNumber = input.required<number>();
   $totalItem = input.required<number>();
   $parPage = input.required<number>();
@@ -61,12 +67,16 @@ export class PaginationComponent {
 
   @Output() setPageNumber = new EventEmitter<number>();
 
+  private _renderer = inject(Renderer2);
+
   public $totalPage = signal(0);
   public $startPage = signal(0);
   public $endPage = signal(0);
 
   public faAngleRight = faAngleRight;
   public faAngleLeft = faAngleLeft;
+
+  private _domReady = false;
 
   constructor() {
     effect(
@@ -89,11 +99,81 @@ export class PaginationComponent {
         );
 
         if (this.$startPage() <= 0) this.$startPage.set(1);
+
+        /* if (this._isFirstChange) {
+          this._isFirstChange = false;
+        } else {
+        } */
+        if (this._domReady) {
+          this.createBtn();
+        }
         // console.log(`El valor de totalPage es: ${this.$totalPage()}`);
         // console.log(`El valor de startPage es: ${this.$startPage()}`);
         // console.log(`El valor de endPage es: ${this.$endPage()}`);
       },
       { allowSignalWrites: true }
     );
+  }
+
+  ngAfterViewInit(): void {
+    this._domReady = true;
+  }
+
+  private createBtn(): void {
+    const elementRef = this.btns.nativeElement.querySelector('#first');
+
+    if (!elementRef) {
+      console.log('No existe el elemento');
+
+      return;
+    }
+    /* let elementRef = '';
+
+    if (this.$pageNumber() > 1) {
+      elementRef = this.btns.nativeElement.querySelector('#first');
+    }
+
+    if (this.$pageNumber() < this.$totalPage()) {
+      elementRef = this.btns.nativeElement.querySelector('#last');
+    } */
+
+    for (let i = this.$startPage(); i < this.$endPage(); i++) {
+      const li = this._renderer.createElement('li');
+      const text = this._renderer.createText(i.toString());
+
+      if (this.$pageNumber() === i) {
+        this._renderer.setAttribute(
+          li,
+          'class',
+          'bg-indigo-300 shadow-lg shadow-indigo-300/50 text-white'
+        );
+      } else {
+        this._renderer.setAttribute(
+          li,
+          'class',
+          'bg-slate-600 hover:bg-indigo-400 shadow-lg hover:shadow-indigo-500/50 hover:text-white text-[#d0d2d6]'
+        );
+      }
+
+      this._renderer.setAttribute(
+        li,
+        'class',
+        'w-[33px] h-[33px] rounded-full flex justify-center items-center cursor-pointer'
+      );
+
+      this._renderer.appendChild(li, text);
+
+      // this.btns.element.nativeElement.appendChild(li);
+
+      if (this.$pageNumber() > 1) {
+        this._renderer.insertBefore(this.btns.nativeElement, li, elementRef);
+      }
+
+      if (this.$pageNumber() < this.$totalPage()) {
+        this._renderer.insertBefore(this.btns.nativeElement, li, elementRef);
+      }
+
+      // this._renderer.insertBefore(this.btns.nativeElement, li, elementRef);
+    }
   }
 }
