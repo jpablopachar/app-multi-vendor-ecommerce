280f6eb1ddef9eb6fd1e060ba182bef8c1a03e7d
jpablopachar <jpablopachar1993@gmail.com>
2024-09-10 18:59:33 -0500

    Se agrega SellerReducer

---
+++ client/src/store/reducers/sellerReducer.js
@@ -0,0 +1,87 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import api from '../../api/api'
+
+export const getSellerRequest = createAsyncThunk(
+  'product/getSellerRequest',
+  async (
+    { parPage, page, searchValue },
+    { rejectWithValue, fulfillWithValue }
+  ) => {
+    try {
+      const { data } = await api.get(
+        `/request-seller-get?page=${page}&&searchValue=${searchValue}&&parPage=${parPage}`,
+        {
+          withCredentials: true,
+        }
+      )
+
+      return fulfillWithValue(data)
+    } catch (error) {
+      return rejectWithValue(error.response.data)
+    }
+  }
+)
+
+export const getSeller = createAsyncThunk(
+  'product/getSeller',
+  async (sellerId, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.get(`/get-seller/${sellerId}`, {
+        withCredentials: true,
+      })
+
+      return fulfillWithValue(data)
+    } catch (error) {
+      return rejectWithValue(error.response.data)
+    }
+  }
+)
+
+export const sellerStatusUpdate = createAsyncThunk(
+  'product/sellerStatusUpdate',
+  async (info, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.post(`/seller-status-update`, info, {
+        withCredentials: true,
+      })
+
+      return fulfillWithValue(data)
+    } catch (error) {
+      return rejectWithValue(error.response.data)
+    }
+  }
+)
+
+export const sellerReducer = createSlice({
+  name: 'seller',
+  initialState: {
+    successMessage: '',
+    errorMessage: '',
+    loader: false,
+    sellers: [],
+    seller: '',
+    totalSellers: 0,
+  },
+  reducers: {
+    messageClear: (state) => {
+      state.errorMessage = ''
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getSellerRequest.fulfilled, (state, { payload }) => {
+        state.sellers = payload.sellers
+        state.totalSellers = payload.totalSellers
+      })
+      .addCase(getSeller.fulfilled, (state, { payload }) => {
+        state.seller = payload.seller
+      })
+      .addCase(sellerStatusUpdate.fulfilled, (state, { payload }) => {
+        state.seller = payload.seller
+        state.successMessage = payload.message
+      })
+  },
+})
+
+export const { messageClear } = sellerReducer.actions
+export default sellerReducer.reducer
--- client/src/store/rootReducers.js
+++ client/src/store/rootReducers.js
@@ -1,11 +1,13 @@
 import authReducer from "./reducers/authReducer"
 import categoryReducer from "./reducers/categoryReducer"
 import productReducer from "./reducers/productReducer"
+import sellerReducer from "./reducers/sellerReducer"
 
 const rootReducer = {
   auth: authReducer,
   category: categoryReducer,
-  product: productReducer
+  product: productReducer,
+  seller: sellerReducer,
 }
 
 export default rootReducer
