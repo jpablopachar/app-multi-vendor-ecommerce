b31b09478e034cfbbd04d9f17051efc89a815c7c
jpablopachar <jpablopachar1993@gmail.com>
2024-09-11 01:31:47 -0500

    Vista SellerDetails y SellerRequest

--- client/src/views/admin/SellerRequest.jsx
+++ client/src/views/admin/SellerRequest.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useEffect, useState } from 'react'
 import { FaEye } from 'react-icons/fa'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { getSellerRequest } from '../../store/Reducers/sellerReducer'
+import Search from '../components/Search'
 import Pagination from '../Pagination'
 
 const SellerRequest = () => {
+  const dispatch = useDispatch()
+
+  const { sellers } = useSelector((state) => state.seller)
+
   const [currentPage, setCurrentPage] = useState(1)
-  // const [searchValue, setSearchValue] = useState('')
+  const [searchValue, setSearchValue] = useState('')
   const [parPage, setParPage] = useState(5)
   // const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    dispatch(getSellerRequest({ parPage, searchValue, page: currentPage }))
+  }, [parPage, searchValue, currentPage])
 
   return (
     <div className="px-2 lg:px-7 pt-5">
       <h1 className="text-[20px] font-bold mb-3"> Seller Request </h1>
+
       <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
-        <div className="flex justify-between items-center">
-          <select
-            onChange={(e) => setParPage(parseInt(e.target.value))}
-            className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
-          >
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
-          </select>
-          <input
-            className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
-            type="text"
-            placeholder="search"
-          />
-        </div>
+        <Search
+          setParPage={setParPage}
+          setSearchValue={setSearchValue}
+          searchValue={searchValue}
+        />
         <div className="relative overflow-x-auto">
           <table className="w-full text-sm text-left text-[#d0d2d6]">
             <thead className="text-sm text-[#d0d2d6] uppercase border-b border-slate-700">
@@ -53,38 +57,38 @@ const SellerRequest = () => {
               </tr>
             </thead>
             <tbody>
-              {[1, 2, 3, 4, 5].map((d, i) => (
+              {sellers.map((d, i) => (
                 <tr className="border-b border-slate-700" key={i}>
                   <td
                     scope="row"
                     className="py-2 px-4 font-medium whitespace-nowrap"
                   >
-                    {d}
+                    {i + 1}
                   </td>
                   <td
                     scope="row"
                     className="py-2 px-4 font-medium whitespace-nowrap"
                   >
-                    Kazi Ariyan{' '}
+                    {d.name}{' '}
                   </td>
                   <td
                     scope="row"
                     className="py-2 px-4 font-medium whitespace-nowrap"
                   >
-                    ariyan@gmail.com
+                    {d.email}{' '}
                   </td>
                   <td
                     scope="row"
                     className="py-2 px-4 font-medium whitespace-nowrap"
                   >
-                    <span>Inactive</span>{' '}
+                    <span>{d.payment}</span>{' '}
                   </td>
 
                   <td
                     scope="row"
                     className="py-2 px-4 font-medium whitespace-nowrap"
                   >
-                    <span>Pending</span>{' '}
+                    <span>{d.status}</span>{' '}
                   </td>
                   <td
                     scope="row"
@@ -92,7 +96,7 @@ const SellerRequest = () => {
                   >
                     <div className="flex justify-start items-center gap-4">
                       <Link
-                        to="/admin/dashboard/seller/details/2"
+                        to={`/admin/dashboard/seller/details/${d._id}`}
                         className="p-[6px] bg-green-500 rounded hover:shadow-lg hover:shadow-green-500/50"
                       >
                         {' '}
