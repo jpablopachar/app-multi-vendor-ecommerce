5066bf4704988f93fe82ceb044d8331754393e3a
jpablopachar <jpablopachar1993@gmail.com>
2024-09-14 09:53:53 -0500

    Se implementa Toaster

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
+import { lazy, StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
 import './index.css'
+
+// eslint-disable-next-line react-refresh/only-export-components
+const App = lazy(() => import('./App'))
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Suspense>
+      <App />
+      <Toaster
+        toastOptions={{
+          position: 'top-right',
+          style: {
+            background: '#283046',
+            color: 'white',
+          },
+        }}
+      />
+    </Suspense>
+  </StrictMode>
 )
