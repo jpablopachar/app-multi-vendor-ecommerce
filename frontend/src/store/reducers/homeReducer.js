d1f90ab53c482d2e8dd437ecf3e058996f54cd21
jpablopachar <jpablopachar1993@gmail.com>
2024-09-17 13:17:49 -0500

    Implementación de Store y homeReducer

---
+++ frontend/src/store/reducers/homeReducer.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const homeReducer = createSlice({
+  name: 'home',
+  initialState: {
+    categories: [],
+  },
+  reducers: {},
+  extraReducers: (builder) => {},
+})
+
+export default homeReducer.reducer
