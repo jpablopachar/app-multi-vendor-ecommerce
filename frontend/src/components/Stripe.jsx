4b4c3dac20fce64f83ce18c0a8b9de24a4b30c2a
jpablopachar <jpablopachar1993@gmail.com>
2024-09-18 23:37:51 -0500

    Diseño del componente CheckoutForm y Stripe

--- frontend/src/components/Stripe.jsx
+++ frontend/src/components/Stripe.jsx
@@ -1,7 +1,25 @@
-const Stripe = () => {
+/* const Stripe = ({ price, orderId }) => {
+  const [clientSecret, setClientSecret] = useState('')
+    const apperance = {
+        theme: 'stripe'
+    }
+    const options = {
+        apperance,
+        clientSecret
+    }
+
+    const create_payment = async () => {
+        try {
+            const { data } = await axios.post('http://localhost:5000/api/order/create-payment',{price},{withCredentials:true})
+            setClientSecret(data.clientSecret)
+        } catch (error) {
+            console.log(error.response.data)
+        }
+    }
+
   return (
     <div>Stripe</div>
   )
 }
 
-export default Stripe
+export default Stripe */
