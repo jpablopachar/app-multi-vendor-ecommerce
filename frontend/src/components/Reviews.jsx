501f9c1329931aa76f45d948c1d4ee8a7d8378b6
jpablopachar <jpablopachar1993@gmail.com>
2024-09-24 02:09:21 -0500

    Funcionalidad en página Reviews y diseño en página ChangePassword

--- frontend/src/components/Reviews.jsx
+++ frontend/src/components/Reviews.jsx
@@ -1,32 +1,90 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
 import { CiStar } from 'react-icons/ci'
 import { FaStar } from 'react-icons/fa'
 import RatingReact from 'react-rating'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import {
+  customerReview,
+  getReviews,
+  messageClear,
+  productDetails,
+} from '../store/reducers/homeReducer'
 import Pagination from './Pagination'
 import Rating from './Rating'
 import RatingTemp from './RatingTemp'
 
-const Reviews = () => {
+const Reviews = ({ product }) => {
+  const dispatch = useDispatch()
+
+  const { userInfo } = useSelector((state) => state.auth)
+
+  const { successMessage, reviews, ratingReview, totalReview } = useSelector(
+    (state) => state.home
+  )
+
   const [parPage, setParPage] = useState(1)
   const [pageNumber, setPageNumber] = useState(10)
   const [rat, setRat] = useState('')
   const [re, setRe] = useState('')
 
-  const userInfo = {}
+  const reviewSubmit = (event) => {
+    event.preventDefault()
+
+    const obj = {
+      name: userInfo.name,
+      rating: rat,
+      review: re,
+      productId: product._id,
+    }
+
+    dispatch(customerReview(obj))
+  }
+
+  useEffect(() => {
+    if (successMessage) {
+      toast.success(successMessage)
+
+      dispatch(
+        getReviews({
+          productId: product._id,
+          pageNumber,
+        })
+      )
+
+      dispatch(productDetails(product.slug))
+
+      setRat('')
+      setRe('')
+
+      dispatch(messageClear())
+    }
+  }, [successMessage])
+
+  useEffect(() => {
+    if (product._id) {
+      dispatch(
+        getReviews({
+          productId: product._id,
+          pageNumber,
+        })
+      )
+    }
+  }, [pageNumber, product])
 
   return (
     <div className="mt-8">
       <div className="flex gap-10 md-lg:flex-col">
         <div className="flex flex-col gap-2 justify-start items-start py-4">
           <div>
-            <span className="text-6xl font-semibold">4.5</span>
+            <span className="text-6xl font-semibold">{product.rating}</span>
             <span className="text-3xl font-semibold text-slate-600">/5</span>
           </div>
           <div className="flex text-3xl">
-            <Rating ratings={4.5} />
+            <Rating ratings={product.rating} />
           </div>
-          <p className="text-sm text-slate-600">15 Reviews</p>
+          <p className="text-sm text-slate-600">({totalReview}) Reviews</p>
         </div>
         <div className="flex gap-2 flex-col py-4">
           <div className="flex justify-start items-center gap-5">
@@ -34,45 +92,90 @@ const Reviews = () => {
               <RatingTemp rating={5} />
             </div>
             <div className="w-[200px] h-[14px] bg-slate-200 relative">
-              <div className="h-full bg-[#Edbb0E] w-[60%]"></div>
+              <div
+                style={{
+                  width: `${Math.floor(
+                    (100 * (ratingReview[0]?.sum || 0)) / totalReview
+                  )}%`,
+                }}
+                className="h-full bg-[#Edbb0E] w-[60%]"
+              ></div>
             </div>
-            <p className="text-sm text-slate-600 w-[0%]">10</p>
+            <p className="text-sm text-slate-600 w-[0%]">
+              {ratingReview[0]?.sum}
+            </p>
           </div>
           <div className="flex justify-start items-center gap-5">
             <div className="text-md flex gap-1 w-[93px]">
               <RatingTemp rating={4} />
             </div>
             <div className="w-[200px] h-[14px] bg-slate-200 relative">
-              <div className="h-full bg-[#Edbb0E] w-[70%]"></div>
+              <div
+                style={{
+                  width: `${Math.floor(
+                    (100 * (ratingReview[1]?.sum || 0)) / totalReview
+                  )}%`,
+                }}
+                className="h-full bg-[#Edbb0E] w-[70%]"
+              ></div>
             </div>
-            <p className="text-sm text-slate-600 w-[0%]">20</p>
+            <p className="text-sm text-slate-600 w-[0%]">
+              {ratingReview[1]?.sum}
+            </p>
           </div>
           <div className="flex justify-start items-center gap-5">
             <div className="text-md flex gap-1 w-[93px]">
               <RatingTemp rating={3} />
             </div>
             <div className="w-[200px] h-[14px] bg-slate-200 relative">
-              <div className="h-full bg-[#Edbb0E] w-[40%]"></div>
+              <div
+                style={{
+                  width: `${Math.floor(
+                    (100 * (ratingReview[2]?.sum || 0)) / totalReview
+                  )}%`,
+                }}
+                className="h-full bg-[#Edbb0E] w-[40%]"
+              ></div>
             </div>
-            <p className="text-sm text-slate-600 w-[0%]">8</p>
+            <p className="text-sm text-slate-600 w-[0%]">
+              {ratingReview[2]?.sum}
+            </p>
           </div>
           <div className="flex justify-start items-center gap-5">
             <div className="text-md flex gap-1 w-[93px]">
               <RatingTemp rating={2} />
             </div>
             <div className="w-[200px] h-[14px] bg-slate-200 relative">
-              <div className="h-full bg-[#Edbb0E] w-[30%]"></div>
+              <div
+                style={{
+                  width: `${Math.floor(
+                    (100 * (ratingReview[3]?.sum || 0)) / totalReview
+                  )}%`,
+                }}
+                className="h-full bg-[#Edbb0E] w-[30%]"
+              ></div>
             </div>
-            <p className="text-sm text-slate-600 w-[0%]">5</p>
+            <p className="text-sm text-slate-600 w-[0%]">
+              {ratingReview[3]?.sum}
+            </p>
           </div>
           <div className="flex justify-start items-center gap-5">
             <div className="text-md flex gap-1 w-[93px]">
               <RatingTemp rating={1} />
             </div>
             <div className="w-[200px] h-[14px] bg-slate-200 relative">
-              <div className="h-full bg-[#Edbb0E] w-[10%]"></div>
+              <div
+                style={{
+                  width: `${Math.floor(
+                    (100 * (ratingReview[4]?.sum || 0)) / totalReview
+                  )}%`,
+                }}
+                className="h-full bg-[#Edbb0E] w-[10%]"
+              ></div>
             </div>
-            <p className="text-sm text-slate-600 w-[0%]">3</p>
+            <p className="text-sm text-slate-600 w-[0%]">
+              {ratingReview[4]?.sum}
+            </p>
           </div>
           <div className="flex justify-start items-center gap-5">
             <div className="text-md flex gap-1 w-[93px]">
@@ -86,35 +189,31 @@ const Reviews = () => {
         </div>
       </div>
       <h2 className="text-slate-600 text-xl font-bold py-5">
-        Product Review 10
+        Product Review ({totalReview})
       </h2>
       <div className="flex flex-col gap-8 pb-10 pt-4">
-        {[1, 2, 3, 4, 5].map((r, i) => (
+        {reviews.map((r, i) => (
           <div key={i} className="flex flex-col gap-1">
             <div className="flex justify-between items-center">
               <div className="flex gap-1 text-xl">
-                <RatingTemp rating={4} />
+                <RatingTemp rating={r.rating} />
               </div>
-              <span className="text-slate-600">8 Jan 2024</span>
+              <span className="text-slate-600">{r.date}</span>
             </div>
-            <span className="text-slate-600 text-md">Kazi Ariyan</span>
-            <p className="text-slate-600 text-sm">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry standard dummy text
-              ever since the 1500s,
-            </p>
+            <span className="text-slate-600 text-md">{r.name}</span>
+            <p className="text-slate-600 text-sm">{r.review}</p>
           </div>
         ))}
         <div className="flex justify-end">
-          {
+          {totalReview > 5 && (
             <Pagination
               pageNumber={pageNumber}
               setPageNumber={setPageNumber}
-              totalItem={10}
+              totalItem={totalReview}
               parPage={parPage}
-              showItem={Math.floor(10 / 3)}
+              showItem={Math.floor(totalReview / 3)}
             />
-          }
+          )}
         </div>
       </div>
       <div>
@@ -136,8 +235,10 @@ const Reviews = () => {
                 }
               />
             </div>
-            <form>
+            <form onSubmit={reviewSubmit}>
               <textarea
+                value={re}
+                onChange={(e) => setRe(e.target.value)}
                 required
                 className="border outline-0 p-3 w-full"
                 name=""
