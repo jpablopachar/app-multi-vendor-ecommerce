3ea825ec92f2b957e99852767ee3823145db19ac
jpablopachar <jpablopachar1993@gmail.com>
2024-09-24 23:21:57 -0500

    Se agrega dashboardReducer

--- client/src/store/rootReducers.js
+++ client/src/store/rootReducers.js
@@ -1,6 +1,7 @@
 import authReducer from "./reducers/authReducer"
 import bannerReducer from "./reducers/bannerReducer"
 import categoryReducer from "./reducers/categoryReducer"
+import dashboardReducer from "./reducers/dashboardReducer"
 import productReducer from "./reducers/productReducer"
 import sellerReducer from "./reducers/sellerReducer"
 
@@ -8,6 +9,7 @@ const rootReducer = {
   auth: authReducer,
   banner: bannerReducer,
   category: categoryReducer,
+  dashboard: dashboardReducer,
   product: productReducer,
   seller: sellerReducer,
 }
