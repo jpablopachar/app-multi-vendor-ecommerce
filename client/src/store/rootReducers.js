02fb6205b9e594639e8f8bf04f008e433b972a37
jpablopachar <jpablopachar1993@gmail.com>
2024-09-24 21:13:30 -0500

    Se agrega bannerReducer a Store

--- client/src/store/rootReducers.js
+++ client/src/store/rootReducers.js
@@ -1,10 +1,12 @@
 import authReducer from "./reducers/authReducer"
+import bannerReducer from "./reducers/bannerReducer"
 import categoryReducer from "./reducers/categoryReducer"
 import productReducer from "./reducers/productReducer"
 import sellerReducer from "./reducers/sellerReducer"
 
 const rootReducer = {
   auth: authReducer,
+  banner: bannerReducer,
   category: categoryReducer,
   product: productReducer,
   seller: sellerReducer,
