2062f2a96e366bfb9b9c3ba7e095f16fdedd7610
jpablopachar <jpablopachar1993@gmail.com>
2024-09-26 01:50:34 -0500

    Se agrega funcionalidad a SellerToAdmin

--- client/src/views/seller/SellerToAdmin.jsx
+++ client/src/views/seller/SellerToAdmin.jsx
@@ -1,4 +1,69 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useEffect, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  getSellerMessage,
+  messageClear,
+  sendMessageSellerAdmin,
+  updateAdminMessage,
+} from '../../store/reducers/chatReducer'
+import { socket } from '../../utils/utils'
+
 const SellerToAdmin = () => {
+  const scrollRef = useRef()
+
+  const dispatch = useDispatch()
+
+  const {
+    sellerAdminMessages,
+    successMessage,
+  } = useSelector((state) => state.chat)
+
+  const { userInfo } = useSelector((state) => state.auth)
+
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+    dispatch(getSellerMessage())
+  }, [])
+
+  useEffect(() => {
+    socket.on('receiverAdminMessage', (msg) => {
+      dispatch(updateAdminMessage(msg))
+    })
+  }, [])
+
+  useEffect(() => {
+    if (successMessage) {
+      socket.emit(
+        'sendMessageSellerToAdmin',
+        sellerAdminMessages[sellerAdminMessages.length - 1]
+      )
+
+      dispatch(messageClear())
+    }
+  }, [successMessage])
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }, [sellerAdminMessages])
+
+  const send = (event) => {
+    event.preventDefault()
+
+    dispatch(
+      sendMessageSellerAdmin({
+        senderId: userInfo._id,
+        receiverId: '',
+        message: text,
+        senderName: userInfo.name,
+      })
+    )
+
+    setText('')
+  }
+
   return (
     <div className="px-2 lg:px-7 py-5">
       <div className="w-full bg-[#6a5fdf] px-4 py-4 rounded-md h-[calc(100vh-140px)]">
@@ -19,52 +84,57 @@ const SellerToAdmin = () => {
             </div>
             <div className="py-4">
               <div className="bg-[#475569] h-[calc(100vh-290px)] rounded-md p-3 overflow-y-auto">
-                <div className="w-full flex justify-start items-center">
-                  <div className="flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]">
-                    <div>
-                      <img
-                        className="w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]"
-                        src="http://localhost:5173/src/assets/images/demo.jpg"
-                        alt=""
-                      />
-                    </div>
-                    <div className="flex justify-center items-start flex-col w-full bg-blue-500 shadow-lg shadow-blue-500/50 text-white py-1 px-2 rounded-sm">
-                      <span>How Are you ? </span>
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full flex justify-end items-center">
-                  <div className="flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]">
-                    <div className="flex justify-center items-start flex-col w-full bg-red-500 shadow-lg shadow-red-500/50 text-white py-1 px-2 rounded-sm">
-                      <span>How Are you ? </span>
-                    </div>
-                    <div>
-                      <img
-                        className="w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]"
-                        src="http://localhost:5173/src/assets/images/admin.jpg"
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full flex justify-start items-center">
-                  <div className="flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]">
-                    <div>
-                      <img
-                        className="w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]"
-                        src="http://localhost:5173/src/assets/images/demo.jpg"
-                        alt=""
-                      />
-                    </div>
-                    <div className="flex justify-center items-start flex-col w-full bg-blue-500 shadow-lg shadow-blue-500/50 text-white py-1 px-2 rounded-sm">
-                      <span>I Need some help </span>
-                    </div>
-                  </div>
-                </div>
+                {sellerAdminMessages.map((m, i) => {
+                  if (userInfo._id === m.senderId) {
+                    return (
+                      <div
+                        ref={scrollRef}
+                        key={i}
+                        className="w-full flex justify-start items-center"
+                      >
+                        <div className="flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]">
+                          <div>
+                            <img
+                              className="w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]"
+                              src="http://localhost:5173/src/assets/images/demo.jpg"
+                              alt=""
+                            />
+                          </div>
+                          <div className="flex justify-center items-start flex-col w-full bg-blue-500 shadow-lg shadow-blue-500/50 text-white py-1 px-2 rounded-sm">
+                            <span>{m.message} </span>
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  } else {
+                    return (
+                      <div
+                        ref={scrollRef}
+                        key={i}
+                        className="w-full flex justify-end items-center"
+                      >
+                        <div className="flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]">
+                          <div className="flex justify-center items-start flex-col w-full bg-red-500 shadow-lg shadow-red-500/50 text-white py-1 px-2 rounded-sm">
+                            <span>{m.message} </span>
+                          </div>
+                          <div>
+                            <img
+                              className="w-[38px] h-[38px] border-2 border-white rounded-full max-w-[38px] p-[3px]"
+                              src="http://localhost:5173/src/assets/images/admin.jpg"
+                              alt=""
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  }
+                })}
               </div>
             </div>
-            <form className="flex gap-3">
+            <form onSubmit={send} className="flex gap-3">
               <input
+                value={text}
+                onChange={(event) => setText(event.target.value)}
                 className="w-full flex justify-between px-2 border border-slate-700 items-center py-[5px] focus:border-blue-500 rounded-md outline-none bg-transparent text-[#d0d2d6]"
                 type="text"
                 placeholder="Input Your Message"
