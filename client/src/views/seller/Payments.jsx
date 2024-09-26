8d132be1baa0a2a4342d3e2d657998a2f8a53e29
jpablopachar <jpablopachar1993@gmail.com>
2024-09-26 01:31:55 -0500

    Se agrega funcionalidad a Payments

--- client/src/views/seller/Payments.jsx
+++ client/src/views/seller/Payments.jsx
@@ -1,6 +1,16 @@
-import { forwardRef } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import moment from 'moment'
+import { forwardRef, useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
 import { MdCurrencyExchange } from 'react-icons/md'
+import { useDispatch, useSelector } from 'react-redux'
 import { FixedSizeList as List } from 'react-window'
+import {
+  getSellerPaymentDetails,
+  messageClear,
+  sendWithdrawRequest,
+} from '../../store/reducers/paymentReducer'
 
 function handleOnWheel({ deltaY }) {
   console.log('handleOnWheel', deltaY)
@@ -12,17 +22,90 @@ const outerElementType = forwardRef((props, ref) => (
 ))
 
 const Payments = () => {
+  const dispatch = useDispatch()
+
+  const { userInfo } = useSelector((state) => state.auth)
+
+  const {
+    successMessage,
+    errorMessage,
+    loader,
+    pendingWithdraws,
+    successWithdraws,
+    totalAmount,
+    withdrawAmount,
+    pendingAmount,
+    availableAmount,
+  } = useSelector((state) => state.payment)
+
+  const [amount, setAmount] = useState(0)
+
+  useEffect(() => {
+    dispatch(getSellerPaymentDetails(userInfo._id))
+  }, [])
+
+  useEffect(() => {
+    if (successMessage) {
+      toast.success(successMessage)
+
+      dispatch(messageClear())
+    }
+
+    if (errorMessage) {
+      toast.error(errorMessage)
+
+      dispatch(messageClear())
+    }
+  }, [successMessage, errorMessage])
+
+  const sendRequest = (event) => {
+    event.preventDefault()
+
+    if (availableAmount - amount > 10) {
+      dispatch(sendWithdrawRequest({ amount, sellerId: userInfo._id }))
+
+      setAmount(0)
+    } else {
+      toast.error('Insufficient Balance')
+    }
+  }
+
   const Row = ({ index, style }) => {
     return (
       <div style={style} className="flex text-sm text-white font-medium">
         <div className="w-[25%] p-2 whitespace-nowrap">{index + 1}</div>
-        <div className="w-[25%] p-2 whitespace-nowrap">$3434</div>
+        <div className="w-[25%] p-2 whitespace-nowrap">
+          ${pendingWithdraws[index]?.amount}
+        </div>
         <div className="w-[25%] p-2 whitespace-nowrap">
           <span className="py-[1px] px-[5px] bg-slate-300 text-blue-500 rounded-md text-sm">
-            Pending
+            {pendingWithdraws[index]?.status}
           </span>
         </div>
-        <div className="w-[25%] p-2 whitespace-nowrap"> 25 Dec 2023 </div>
+        <div className="w-[25%] p-2 whitespace-nowrap">
+          {' '}
+          {moment(pendingWithdraws[index]?.createdAt).format('LL')}{' '}
+        </div>
+      </div>
+    )
+  }
+
+  const Rows = ({ index, style }) => {
+    return (
+      <div style={style} className="flex text-sm text-white font-medium">
+        <div className="w-[25%] p-2 whitespace-nowrap">{index + 1}</div>
+        <div className="w-[25%] p-2 whitespace-nowrap">
+          ${successWithdraws[index]?.amount}
+        </div>
+        <div className="w-[25%] p-2 whitespace-nowrap">
+          <span className="py-[1px] px-[5px] bg-slate-300 text-blue-500 rounded-md text-sm">
+            {successWithdraws[index]?.status}
+          </span>
+        </div>
+        <div className="w-[25%] p-2 whitespace-nowrap">
+          {' '}
+          {moment(successWithdraws[index]?.createdAt).format('LL')}{' '}
+        </div>
       </div>
     )
   }
@@ -32,27 +115,25 @@ const Payments = () => {
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7 mb-5">
         <div className="flex justify-between items-center p-5 bg-[#fae8e8] rounded-md gap-3">
           <div className="flex flex-col justify-start items-start text-[#5c5a5a]">
-            <h2 className="text-2xl font-bold">$3434</h2>
+            <h2 className="text-2xl font-bold">${totalAmount}</h2>
             <span className="text-sm font-bold">Total Sales</span>
           </div>
-
           <div className="w-[40px] h-[47px] rounded-full bg-[#fa0305] flex justify-center items-center text-xl">
             <MdCurrencyExchange className="text-[#fae8e8] shadow-lg" />
           </div>
         </div>
         <div className="flex justify-between items-center p-5 bg-[#fde2ff] rounded-md gap-3">
           <div className="flex flex-col justify-start items-start text-[#5c5a5a]">
-            <h2 className="text-2xl font-bold">$150</h2>
+            <h2 className="text-2xl font-bold">${availableAmount}</h2>
             <span className="text-sm font-bold">Available Amount</span>
           </div>
-
           <div className="w-[40px] h-[47px] rounded-full bg-[#760077] flex justify-center items-center text-xl">
             <MdCurrencyExchange className="text-[#fae8e8] shadow-lg" />
           </div>
         </div>
         <div className="flex justify-between items-center p-5 bg-[#e9feea] rounded-md gap-3">
           <div className="flex flex-col justify-start items-start text-[#5c5a5a]">
-            <h2 className="text-2xl font-bold">$100</h2>
+            <h2 className="text-2xl font-bold">${withdrawAmount}</h2>
             <span className="text-sm font-bold">WithDrawal Amount</span>
           </div>
           <div className="w-[40px] h-[47px] rounded-full bg-[#038000] flex justify-center items-center text-xl">
@@ -61,10 +142,9 @@ const Payments = () => {
         </div>
         <div className="flex justify-between items-center p-5 bg-[#ecebff] rounded-md gap-3">
           <div className="flex flex-col justify-start items-start text-[#5c5a5a]">
-            <h2 className="text-2xl font-bold">$0</h2>
+            <h2 className="text-2xl font-bold">${pendingAmount}</h2>
             <span className="text-sm font-bold">Pending Amount</span>
           </div>
-
           <div className="w-[40px] h-[47px] rounded-full bg-[#0200f8] flex justify-center items-center text-xl">
             <MdCurrencyExchange className="text-[#fae8e8] shadow-lg" />
           </div>
@@ -74,23 +154,27 @@ const Payments = () => {
         <div className="bg-[#6a5fdf] text-[#d0d2d6] rounded-md p-5">
           <h2 className="text-lg">Send Request</h2>
           <div className="pt-5 mb-5">
-            <form>
+            <form onSubmit={sendRequest}>
               <div className="flex gap-3 flex-wrap">
                 <input
+                  onChange={(event) => setAmount(event.target.value)}
+                  value={amount}
                   min="0"
                   type="number"
                   className="px-3 py-2 md:w-[75%] focus:border-indigo-200 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
                   name="amount"
                 />
-                <button className="bg-red-500  hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-2">
-                  Submit
+                <button
+                  disabled={loader}
+                  className="bg-red-500  hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-2"
+                >
+                  {loader ? 'loading..' : 'Submit'}
                 </button>
               </div>
             </form>
           </div>
           <div>
             <h2 className="text-lg pb-4">Pending Request </h2>
-
             <div className="w-full overflow-x-auto">
               <div className="flex bg-[#a7a3de] uppercase text-xs font-bold min-w-[340px] rounded-md">
                 <div className="w-[25%] p-2"> No </div>
@@ -103,7 +187,7 @@ const Payments = () => {
                   style={{ minWidth: '340px' }}
                   className="List"
                   height={350}
-                  itemCount={10}
+                  itemCount={pendingWithdraws.length}
                   itemSize={35}
                   outerElementType={outerElementType}
                 >
@@ -113,7 +197,6 @@ const Payments = () => {
             </div>
           </div>
         </div>
-
         <div className="bg-[#6a5fdf] text-[#d0d2d6] rounded-md p-5">
           <div>
             <h2 className="text-lg pb-4">Success WithDrawal </h2>
@@ -129,11 +212,11 @@ const Payments = () => {
                   style={{ minWidth: '340px' }}
                   className="List"
                   height={350}
-                  itemCount={10}
+                  itemCount={successWithdraws.length}
                   itemSize={35}
                   outerElementType={outerElementType}
                 >
-                  {Row}
+                  {Rows}
                 </List>
               }
             </div>
